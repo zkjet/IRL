d1442d6fc8e98d941d17bfe7469644204c4aec2b
zkjet <zkjet@mailfence.com>
2023-04-05 22:30:18 -0600

    updated hardhat config for etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,43 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+
 import { HardhatUserConfig } from "hardhat/config";
+import { ethers } from "ethers";
+import "@nomiclabs/hardhat-etherscan";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// WALLET
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// INFRASTRUCTURE
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const INFURA_API_SECRET = process.env.INFURA_API_SECRET;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+// BLOCKEXPLORERS
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
+  paths: { tests: "tests" },
   solidity: "0.8.17",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + INFURA_API_KEY,
+      accounts: [PRIVATE_KEY],
+      etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+      },
+    },
+  },
 };
 
 export default config;
